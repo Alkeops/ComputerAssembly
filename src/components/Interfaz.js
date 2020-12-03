3fd44fd6e30e612eed0dc7769115b584a38fab28
Alkeops <m_adan2012@outlook.com>
2020-12-03 13:28:44 -0600

    mixin breakpoint

--- src/components/Interfaz.js
+++ src/components/Interfaz.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Menu from "./Menu";
 import Social from "./Social";
 
 const Interfaz = () => {
+  useEffect(() => {
+    const int = document.querySelector(".interfaz");
+    let vh = window.innerHeight * 0.01;
+    int.style.setProperty("--vh", `${vh}px`);
+  }, []);
   return (
     <div className="interfaz">
       <h1>Computer Assembly</h1>
